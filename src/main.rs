c447d5a3942ad2454c20f734a3407c14615a3f1f
TheRealMothebad <mothebad@gmail.com>
2023-05-21 21:23:15 -0400

    Testing rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/bees")]
+fn bee_mode() -> &'static str {
+    "It's bee time :)"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index]).mount("/", routes![bee_mode])
 }
