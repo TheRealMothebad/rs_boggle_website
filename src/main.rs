5dc7a62e062deb7201cd7c548242a4b5bc6da747
TheRealMothebad <mothebad@gmail.com>
2023-05-24 00:39:26 -0400

    It workey (sending board and validating size/charset serverside)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+use regex::Regex;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -33,13 +34,25 @@ fn handle_connection(mut stream: TcpStream, num: u8) {
 
     let other_stuff = format!("Access-Control-Allow-Origin: *");
 
-    let contents = num.to_string();
+    let re = Regex::new(r"[^a-z]gi").unwrap();
+
+    let contents: &str = match http_request.get(0) {
+        Some(post_rq) 
+            if (post_rq.len() == 31 && !re.is_match(&post_rq[6..22])) => scrape_board(&post_rq[6..22]),
+        Some(_) => "Error: Incomplete Table",
+        _ => "Error: Generic"
+    };
+
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\n{other_stuff}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\n{other_stuff}\r\nContent-Length: {length}\r\n\r\n{contents}");
     
     println!("Response:\n{}", response);
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+//board is guaranteed to be 16 characters and only containing a-z or A-Z
+fn scrape_board(board: &str) -> &str{
+    &"it workey"
 }
